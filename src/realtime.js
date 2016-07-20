285c0b2200c013bdeeb8dd1743421dbad8fafe51
ydfzgyj <ydfzgyj@baliqieluo.com>
2016-07-20 19:23:24 +0800

    users should import leancloud-realtime by themselves

--- src/realtime.js
+++ src/realtime.js
@@ -2,12 +2,6 @@
 let Realtime;
 if (typeof AV === 'object' && AV.TypedMessage) {
   Realtime = AV;
-} else if (typeof require === 'function') {
-  try {
-    Realtime = require('leancloud-realtime'); // eslint-disable-line
-  } catch (e) {
-    throw new Error('peerDependency \'leancloud-realtime\' not found, install it first');
-  }
 } else {
   throw new Error('AV.Realtime not exists, import LeanCloud Realtime SDK first');
 }
